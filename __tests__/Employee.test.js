ec8139d5c462b83019075953e50473e65ee8463b
xunvyre <victoria@thedavids.us>
2022-08-14 15:18:00 -0400

    employee methods added and tested, jest downloaded

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,20 @@
+const Employee = require('../__lib__/Employee');
+
+test('creates new employee', () =>
+{
+    const employee = new Employee('Meatloaf G. Pig', 1234, 'stinkman@gmail.com');
+
+    expect(employee.name).toBe("Meatloaf G. Pig");
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+});
+
+test('checks all methods for Employee class', () =>
+{
+    const employee = new Employee('Meatloaf G. Pig', 1234, 'stinkman@gmail.com');
+
+    expect(employee.getName()).toBe(employee.name);
+    expect(employee.getId()).toBe(employee.id);
+    expect(employee.getEmail()).toBe(employee.email);
+    expect(employee.getRole()).toBe('Employee');
+});
